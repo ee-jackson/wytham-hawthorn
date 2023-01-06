6aeceb082d460e891e5da24bfc41fe745e2915cd
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2023-01-06 18:42:11 +0000

    switched to half formatted files as input
    Matt edited the raw files manually. Changes made between raw and half formatted are:
    Making name = 0 for the focal tree
    Splitting the status column which includes D, Q, H into three columns that are named D, Q and H
    Adding a column for Susie’s trees – taken from comments
    Standardising all measurements i.e. all are now DBH rather than circumference

--- code/scripts/00_clean-mapping-data.R
+++ code/scripts/00_clean-mapping-data.R
@@ -17,6 +17,8 @@ library("here")
 googledrive::drive_auth(email = "eleanor.elizabeth.j@gmail.com",
                         scopes = "https://www.googleapis.com/auth/drive")
 
+source(here::here("code", "secrets.R"))
+
 files <- drive_ls(GDRIVE_FOLDER_URL,
                   pattern = "^[0-9]{2}.csv")
 
@@ -30,66 +32,58 @@ data_list <- lapply(urls, read.csv, na.strings = c("","NA"))
 
 # Tidy column names -------------------------------------------------------
 
-repair_col_names <- function (plot) {
+repair_cols <- function (plot) {
   plot %>%
-    rename_with(recode, "BHD" = "DBH") %>%
-    rename_with(recode, "BHC" = "CBH") %>%
-    rowwise() %>%
-    mutate(DBH = ifelse("DBH" %in% names(.), DBH, NA),
-           CBH = ifelse("CBH" %in% names(.), CBH, NA),
-           Other.HC = ifelse("Other.HC" %in% names(.), Other.HC, NA)) %>%
-    ungroup() %>%
-    mutate(across(c(DBH, CBH, Other.HC, Other.HD), as.numeric)) %>%
-    mutate(Notes = as.character(Notes)) %>%
-    select(-contains("X"))
+    clean_names() %>%
+    mutate(across(c(dbh, other_d), as.numeric)) %>%
+    mutate(notes = as.character(notes))
 }
 
-data_list_col_repair <- lapply(data_list, repair_col_names)
-
-# Tidy tree IDs -----------------------------------------------------------
-
-repair_tree_names <- function (plot, plot_name) {
-  plot %>%
-    # remove any letters
-    mutate(Name = gsub("[a-zA-Z]+", "", x = Name, ignore.case = TRUE)) %>%
-    # remove leading zeros
-    mutate(Name = gsub("^0+", "", Name) ) %>%
-    # rename 1st row as focal tree
-    mutate(Name = replace(Name, pluck(1, 1),
-                          paste0("focal_", readr::parse_number(plot_name)))) %>%
-    mutate(Name = as.factor(Name))
-}
-
-data_list_name_repair <- imap(data_list_col_repair, repair_tree_names)
+data_list_col_repair <- lapply(data_list, repair_cols)
 
 # Tidy rows ---------------------------------------------------------------
 
-all_plots <- bind_rows(data_list_name_repair, .id = "plot")
+all_plots <- bind_rows(data_list_col_repair, .id = "plot")
 
 all_plots %>%
-  clean_names() %>%
-  fill(c("name", "longitude", "latitude", "solution_status",
-         "cs_name", "form", "reproductive")) %>%
+  select(-name_1, -description) %>%
   mutate(plot = gsub(".csv", "", x = plot)) %>%
-  rename(tree_id = name) %>%
+  rename(
+    tree_id = name,
+    dead = d,
+    horizontal = h,
+    broken = q,
+    susie_id = susie
+  ) %>%
+  mutate(susie_id = case_when(!is.na(tree_id) ~ susie_id,
+                              TRUE ~ NA_character_)) %>%
+  fill(
+    c("tree_id", "longitude", "latitude", "solution_status", "cs_name",
+      "form","reproductive", "susie_id")
+  ) %>%
   group_by(plot) %>%
   mutate(stem_id = 1:n()) %>%
   ungroup() %>%
   add_count(tree_id, plot, name = "n_stems") %>%
-  rowwise() %>%
-  mutate(dbh = ifelse(is.na(dbh), cbh/pi, dbh)) %>%
-  ungroup() %>%
-  mutate(status = ifelse(is.na(status), "A", status)) %>%
   mutate_if(is.character, tolower) %>%
-  mutate(tree_id = paste("tree", tree_id, sep = "_"),
-         stem_id = paste("stem", stem_id, sep = "_")) -> clean_plots
+  mutate(across(
+    c(dead, horizontal, broken),
+    ~ case_when(.x == "y" ~ TRUE, .x == "n" ~ FALSE, TRUE ~ NA)
+  )) %>%
+  mutate(susie_id = dplyr::na_if(susie_id, "n")) %>%
+  mutate(cs_name = gsub(" ", "_", cs_name) ) %>%
+  mutate(
+    tree_id = paste("tree", tree_id, sep = "_"),
+    stem_id = paste("stem", stem_id, sep = "_")
+  ) -> clean_plots
 
 # only keep the largest stem of a multi-stemmed tree
 clean_plots %>%
   drop_na(dbh) %>% # this will drop dbh that were below breast height
-  filter(status != "D") %>%
+  filter(dead == FALSE) %>%
   group_by(plot, tree_id) %>%
   slice_max(dbh, with_ties = FALSE) %>%
-  ungroup() -> largest_stems
+  ungroup() %>%
+  select(- other_d, - split_height) -> largest_stems
 
 saveRDS(largest_stems, file = here::here("data", "clean", "hawthorn_plots.rds"))
