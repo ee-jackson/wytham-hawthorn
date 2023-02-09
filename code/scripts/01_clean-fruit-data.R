76b8dd933f462ed1366b11bf1de5e146bc704f2c
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2023-02-09 17:00:14 +0000

    Create 01_clean-fruit-data.R

---
+++ code/scripts/01_clean-fruit-data.R
@@ -0,0 +1,56 @@
+#!/usr/bin/env Rscript
+
+## Author: E E Jackson, eleanor.elizabeth.j@gmail.com
+## Script: clean-fruit-data
+## Desc:
+## Date created: 2023-02-07
+
+# packages ----------------------------------------------------------------
+
+library("here")
+library("tidyverse")
+library("lubridate")
+
+
+# get data ----------------------------------------------------------------
+
+fruit_counts <- read.csv(here::here("data", "raw", "fruit_counts.csv"),
+  header = TRUE, na.strings = c("", "NA"))
+
+
+# clean -------------------------------------------------------------------
+
+fruit_counts %>%
+  mutate(date = dmy(date), branch = str_sub(branch_id, -1)) %>%
+  mutate(branch = tolower(branch)) %>%
+  mutate(branch_id = paste0(tree_id, branch)) %>%
+  mutate(survey = case_when(
+    date == "2022/03/04" | date == "2022/03/06" | date == "2022/03/07" ~ 8,
+    date == "2022/01/28" | date == "2022/01/27" ~ 7,
+    date == "2021/12/21" | date == "2021/12/20" ~ 6,
+    date == "2021/11/25" | date == "2021/11/24" ~ 5,
+    date == "2021/10/29" | date == "2021/10/28" ~ 4,
+    date == "2021/09/29" | date == "2021/09/28" | date == "2021/09/27" ~ 3,
+    date == "2021/09/02" ~ 2,
+    date == "2021/08/06" | date == "2021/08/05" ~ 1,
+  )) %>%
+  mutate(exclusion = ifelse(branch == "d" | branch == "e" | branch == "f",
+                            FALSE, TRUE)) %>%
+  group_by(branch_id) %>%
+  summarise(across(),
+            length_cm = median(length_cm, na.rm = TRUE),
+            .groups = "drop") %>%
+  select(- branch, - notes) -> survey_data
+
+
+# summarise ---------------------------------------------------------------
+
+# Tree 20 died and we only got 2 time-points of data, I drop it here
+survey_data %>%
+  group_by(branch_id) %>%
+  summarise(across(c(tree_id, exclusion, length_cm)),
+            total_fruit = max(n_fruit),
+            n_dropped = max(n_fruit) - min(n_fruit), .groups = "drop") %>%
+  mutate(proportion_dropped = n_dropped / total_fruit) %>%
+  filter(tree_id != "tree_20") %>%
+  saveRDS(here::here("data", "raw", "fruit_drop_data.rds"))
