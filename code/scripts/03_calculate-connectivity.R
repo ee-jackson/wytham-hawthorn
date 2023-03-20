73ecf9aaf3b506ccaca71010584fa5afcb8785cc
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2023-03-20 10:50:32 +0000

    get connectivity for reproductive trees

--- code/scripts/03_calculate-connectivity.R
+++ code/scripts/03_calculate-connectivity.R
@@ -18,7 +18,10 @@ library("geosphere")
 readRDS(here::here("data", "clean", "hawthorn_plots.rds")) %>%
   group_split(plot) -> plot_dfs
 
-readRDS(here::here("data", "clean", "fruit_drop_data.rds")) -> fruit_data
+readRDS(here::here("data", "clean", "hawthorn_plots.rds")) %>%
+  filter(tree_id == "tree_0" | reproductive == "y") %>%
+  group_split(plot) -> plot_dfs_repro
+
 
 # Calculate pairwise distances --------------------------------------------
 
@@ -38,9 +41,16 @@ calculate_dist <- function (data) {
 
 }
 
+# all trees
 dist_dfs <- lapply(plot_dfs, calculate_dist)
 
 dist_dfs_focal <- map(dist_dfs, ~select(.x, plot, tree_id, tree_0, dbh))
+
+# reproductive trees only
+dist_dfs_repro <- lapply(plot_dfs_repro, calculate_dist)
+
+dist_dfs_focal_repro <- map(dist_dfs_repro,
+                            ~select(.x, plot, tree_id, tree_0, dbh))
 
 
 # Calculate connectivity --------------------------------------------------
@@ -50,22 +60,30 @@ calculate_connectivity <- function (data) {
     filter(tree_id != "tree_0") %>%
     rename(distance = tree_0) %>%
     filter(distance <= 50) %>%
-    mutate(x = exp(- 0.02 * distance ) * dbh ) %>%
+    mutate(x = exp(- 0.02 * distance ) * dbh^(0.5) ) %>%
     summarise(connectivity = sum(x),
               plot = plot) %>%
     distinct()
 }
 
+# all trees
 connectivity_dfs <- lapply(dist_dfs_focal, calculate_connectivity)
 
 connectivity_dfs %>%
   dplyr::bind_rows() %>%
   mutate(plot = as.numeric(plot)) -> all_connectivity_dfs
 
+# reproductive trees only
+connectivity_dfs_repro <- lapply(dist_dfs_focal_repro, calculate_connectivity)
 
-# Combine with fruit data -------------------------------------------------
+connectivity_dfs_repro %>%
+  dplyr::bind_rows() %>%
+  mutate(plot = as.numeric(plot)) -> all_connectivity_dfs_repro
 
-fruit_data %>%
-  right_join(all_connectivity_dfs, by = c("tree_id" = "plot")) %>%
-  mutate(tree_id = as.factor(tree_id)) %>%
+
+# Combine and save --------------------------------------------------------
+
+all_connectivity_dfs_repro %>%
+  rename(repro_connectivity = connectivity) %>%
+  left_join(all_connectivity_dfs, by = "plot") %>%
   saveRDS(here::here("data", "clean", "connectivity_data.rds"))
