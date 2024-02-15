0036984555cc61ab5f1589b123125db174059301
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2024-02-15 11:21:12 +0000

    re-run for non-repo models

--- code/scripts/05_check-models.R
+++ code/scripts/05_check-models.R
@@ -68,21 +68,21 @@ get_table <- function(model) {
 
 # Plot for fruit drop -----------------------------------------------------
 
-drop_r_t <- get_table(model_list$fruit_drop_fit.rds)
+drop_r_t <- get_table(model_list$fruit_drop_repro_fit.rds)
 gtsave(drop_r_t, here::here("output", "results", "fruit_drop_repro.png"))
 drop_r_t_png <- png::readPNG(here::here("output", "results", "fruit_drop_repro.png"),
                          native = TRUE)
 
-drop_r_pp <- plot_pp_check(model_list$fruit_drop_fit.rds)
+drop_r_pp <- plot_pp_check(model_list$fruit_drop_repro_fit.rds)
 
-drop_t_t <- get_table(model_list$fruit_drop_tot_fit.rds)
-gtsave(drop_t_t, here::here("output", "results", "fruit_drop_total.png"))
-drop_t_t_png <- png::readPNG(here::here("output", "results", "fruit_drop_total.png"),
+drop_n_t <- get_table(model_list$fruit_drop_non_fit.rds)
+gtsave(drop_n_t, here::here("output", "results", "fruit_drop_non.png"))
+drop_n_t_png <- png::readPNG(here::here("output", "results", "fruit_drop_non.png"),
                          native = TRUE)
 
-drop_t_pp <- plot_pp_check(model_list$fruit_drop_tot_fit.rds)
+drop_n_pp <- plot_pp_check(model_list$fruit_drop_non_fit.rds)
 
-((drop_r_pp / drop_r_t_png) | (drop_t_pp / drop_t_t_png)) +
+((drop_r_pp / drop_r_t_png) | (drop_n_pp / drop_n_t_png)) +
   plot_annotation(tag_levels = 'a') &
   theme(plot.tag = element_text(size = 20))
 
@@ -103,14 +103,14 @@ set_r_t_png <- png::readPNG(here::here("output", "results", "fruit_set_repro.png
 
 set_r_pp <- plot_pp_check(model_list$fruit_set_repro_fit.rds)
 
-set_t_t <- get_table(model_list$fruit_set_tot_fit.rds)
-gtsave(set_t_t, here::here("output", "results", "fruit_set_total.png"))
-set_t_t_png <- png::readPNG(here::here("output", "results", "fruit_set_total.png"),
+set_n_t <- get_table(model_list$fruit_set_non_fit.rds)
+gtsave(set_n_t, here::here("output", "results", "fruit_set_non.png"))
+set_n_t_png <- png::readPNG(here::here("output", "results", "fruit_set_non.png"),
                            native = TRUE)
 
-set_t_pp <- plot_pp_check(model_list$fruit_set_tot_fit.rds)
+set_n_pp <- plot_pp_check(model_list$fruit_set_non_fit.rds)
 
-((set_r_pp / set_r_t_png) | (set_t_pp / set_t_t_png)) +
+((set_r_pp / set_r_t_png) | (set_n_pp / set_n_t_png)) +
   plot_annotation(tag_levels = 'a') &
   theme(plot.tag = element_text(size = 20))
 
@@ -124,21 +124,21 @@ png(
 
 # Plot for short fruit drop -----------------------------------------------
 
-drops_r_t <- get_table(model_list$fruit_drop_short_fit.rds)
-gtsave(drops_r_t, here::here("output", "results", "fruit_drops_repro.png"))
-drops_r_t_png <- png::readPNG(here::here("output", "results", "fruit_drops_repro.png"),
+drops_r_t <- get_table(model_list$fruit_drop_short_repro_fit.rds)
+gtsave(drops_r_t, here::here("output", "results", "fruit_drop_short_repro.png"))
+drops_r_t_png <- png::readPNG(here::here("output", "results", "fruit_drop_short_repro.png"),
                             native = TRUE)
 
-drops_r_pp <- plot_pp_check(model_list$fruit_drop_short_fit.rds)
+drops_r_pp <- plot_pp_check(model_list$fruit_drop_short_repro_fit.rds)
 
-drops_t_t <- get_table(model_list$fruit_drop_short_tot_fit.rds)
-gtsave(drops_t_t, here::here("output", "results", "fruit_drops_total.png"))
-drops_t_t_png <- png::readPNG(here::here("output", "results", "fruit_drops_total.png"),
+drops_n_t <- get_table(model_list$fruit_drop_short_non_fit.rds)
+gtsave(drops_n_t, here::here("output", "results", "fruit_drop_short_non.png"))
+drops_n_t_png <- png::readPNG(here::here("output", "results", "fruit_drop_short_non.png"),
                             native = TRUE)
 
-drops_t_pp <- plot_pp_check(model_list$fruit_drop_short_tot_fit.rds)
+drops_n_pp <- plot_pp_check(model_list$fruit_drop_short_non_fit.rds)
 
-((drops_r_pp / drops_r_t_png) | (drops_t_pp / drops_t_t_png)) +
+((drops_r_pp / drops_r_t_png) | (drops_n_pp / drops_n_t_png)) +
   plot_annotation(tag_levels = 'a') &
   theme(plot.tag = element_text(size = 20))
 
